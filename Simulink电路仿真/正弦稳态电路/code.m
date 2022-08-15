6b44d7ad38f2138aa63f266b82e3aedf87de7bb1
comtechco <comtechco@icloud.com>
2022-08-15 21:59:06 +0800

    完成 正弦稳态电路

---
+++ Simulink电路仿真/正弦稳态电路/code.m
@@ -0,0 +1,14 @@
+t = 32:0.001:50;
+k = 6;
+it = 2*sqrt(2).*cos(k*t);
+Req = 1/((1/1)+(1/(-i*0.5))+(1/i));
+ut = 2*sqrt(2)*abs(Req).*cos(k*t+angle(Req))/10;
+figure;
+subplot(2,1,2);
+plot(t,it);
+xlabel('t/s')
+ylabel('i(t)/A')
+subplot(2,1,1);
+plot(t,ut);
+xlabel('t/s')
+ylabel('U(t)/v')
